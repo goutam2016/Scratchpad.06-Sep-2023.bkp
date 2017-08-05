e48f027dca2f76604e15a8dabc32ebb80d0af6ce
goutam2016 <gb_info_2007@yahoo.com>
2017-08-05 23:50:10 +0100

    New person-profile data file with 50000 records.

--- Hadoop/simple-jobs/src/test/java/org/gb/sample/hadoop/income/NameVsIncomeDataGenerator.java
+++ Hadoop/simple-jobs/src/test/java/org/gb/sample/hadoop/income/NameVsIncomeDataGenerator.java
@@ -10,11 +10,24 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class NameVsIncomeDataGenerator {
 
+	private static final String DEFAULT_PERSON_PROFILE_FILENAME = "input/income/person-profile.txt";
+	private static final String DEFAULT_NAME_VS_INCOME_FILENAME = "input/income/name-vs-income.txt";
+
 	public static void main(String[] args) {
-		final String personProfileFileName = "input/income/person-profile.txt";
-		final String nameVsIncomeFileName = "input/income/name-vs-income.txt";
+		String personProfileFileName = DEFAULT_PERSON_PROFILE_FILENAME;
+		String nameVsIncomeFileName = DEFAULT_NAME_VS_INCOME_FILENAME;
+
+		if (args.length == 2) {
+			personProfileFileName = args[0];
+			nameVsIncomeFileName = args[1];
+		}
+
 		try {
 			List<PersonProfile> personProfiles = loadPersonProfiles(personProfileFileName);
 			generateNameVsIncomeRecords(personProfiles, nameVsIncomeFileName);
@@ -26,7 +39,7 @@ public class NameVsIncomeDataGenerator {
 	private static void generateNameVsIncomeRecords(List<PersonProfile> personProfiles, String nameVsIncomeFileName)
 			throws IOException {
 		final int baseIncome = 20000;
-		final int maxVarIncome = 60000;
+		final int maxVarIncome = 120000;
 
 		Path nameVsIncomeFilePath = Paths.get(nameVsIncomeFileName);
 		PrintWriter writer = new PrintWriter(Files.newBufferedWriter(nameVsIncomeFilePath), true);
@@ -42,22 +55,41 @@ public class NameVsIncomeDataGenerator {
 		writer.close();
 	}
 
+	private static CSVRecord convertToCSVRecord(String lineOfText) {
+		CSVRecord csvRecord = null;
+		try {
+			List<CSVRecord> csvRecords = CSVParser.parse(lineOfText, CSVFormat.DEFAULT).getRecords();
+			csvRecord = csvRecords.get(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return csvRecord;
+	}
+
 	private static PersonProfile convertLineToPersProfile(String persProfileLine) {
-		String[] profileValues = persProfileLine.split(",");
+		CSVRecord persProfileRecord = convertToCSVRecord(persProfileLine);
+
+		if (persProfileRecord == null) {
+			return null;
+		}
 
 		PersonProfile profile = new PersonProfile();
-		profile.setFirstName(profileValues[0].replace("\"", ""));
-		profile.setLastName(profileValues[1].replace("\"", ""));
-		profile.setCompanyName(profileValues[2].replace("\"", ""));
-		profile.setAddress(profileValues[3].replace("\"", ""));
-		profile.setCity(profileValues[4].replace("\"", ""));
-		profile.setCounty(profileValues[5].replace("\"", ""));
-		profile.setPostCode(profileValues[6].replace("\"", ""));
-		profile.setPhoneNumber1(profileValues[7].replace("\"", ""));
-		profile.setPhoneNumber2(profileValues[8].replace("\"", ""));
-		profile.setEmailAddress(profileValues[9].replace("\"", ""));
-		profile.setWebsite(profileValues[10].replace("\"", ""));
 
+		profile.setFirstName(persProfileRecord.get(0));
+		profile.setLastName(persProfileRecord.get(1));
+		profile.setCompanyName(persProfileRecord.get(2));
+		profile.setAddress(persProfileRecord.get(3));
+		profile.setCity(persProfileRecord.get(4));
+		profile.setCounty(persProfileRecord.get(5));
+		profile.setPostCode(persProfileRecord.get(6));
+		profile.setPhoneNumber1(persProfileRecord.get(7));
+		profile.setPhoneNumber2(persProfileRecord.get(8));
+		profile.setEmailAddress(persProfileRecord.get(9));
+		profile.setWebsite(persProfileRecord.get(10));
+		System.out.println(String.format(
+				"Parsed with commons-csv: first-name: %s, last-name: %s, company: %s, address: %s, city: %s, post-code: %s.",
+				profile.getFirstName(), profile.getLastName(), profile.getCompanyName(), profile.getAddress(),
+				profile.getCity(), profile.getPostCode()));
 		return profile;
 	}
 
