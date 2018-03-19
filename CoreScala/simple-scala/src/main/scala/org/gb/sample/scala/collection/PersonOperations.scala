d12e1e65ddba7598c8790d8b5e9527a3f7bedc87
Goutam Bhattacharjee <gbhattacharjee@apple.com>
2018-03-19 01:06:21 +0000

    More operations on Person/Employee Scala collections.

--- CoreScala/simple-scala/src/main/scala/org/gb/sample/scala/collection/PersonOperations.scala
+++ CoreScala/simple-scala/src/main/scala/org/gb/sample/scala/collection/PersonOperations.scala
@@ -17,4 +17,32 @@ object PersonOperations {
         val millisPerDay = 24 * 60 * 60 * 1000
         employees.filter(_.getJoiningDate() != null).map(System.currentTimeMillis() - _.getJoiningDate().getTime).map(_ / millisPerDay).sum
     }
+
+    def getTotalExperienceOfFemaleEmployees(employees: IndexedSeq[Employee]): Long = {
+        val millisPerDay = 24 * 60 * 60 * 1000
+        employees.filter(_.getSex() == Sex.FEMALE).filter(_.getJoiningDate() != null).map(System.currentTimeMillis() - _.getJoiningDate().getTime).map(_ / millisPerDay).sum
+    }
+
+    def maxSalaryPerDept(employees: IndexedSeq[Employee]): Map[String, BigDecimal] = {
+        employees.groupBy(_.getDepartment()).mapValues(_.map(_.getYearlyGrossSalary())).mapValues(_.max)
+    }
+
+    def empNameWithMaxSalaryPerDept(employees: IndexedSeq[Employee]): Map[String, String] = {
+        employees.groupBy(_.getDepartment()).mapValues(_.maxBy(_.getYearlyGrossSalary())).mapValues(emp => String.join(" ", emp.getFirstName(), emp.getLastName()))
+    }
+
+    def employeeCountPerSexPerDept(employees: IndexedSeq[Employee], baseSalary: BigDecimal): Map[String, Map[String, Int]] = {
+        val sexClassifier = (emp: Employee) => emp.getSex() match {
+            case Sex.MALE => "MALE_COUNT"
+            case Sex.FEMALE => "FEMALE_COUNT"
+        }
+        val employeesPerSexPerDept = employees.filter(_.getYearlyGrossSalary() >= baseSalary).groupBy(_.getDepartment()).mapValues(_.groupBy(sexClassifier))
+        employeesPerSexPerDept.mapValues(_.mapValues(_.size))
+    }
+
+    def averageSalaryPerDepartment(employees: IndexedSeq[Employee]): Map[String, BigDecimal] = {
+        val salaryAdder = (salary1: BigDecimal, salary2: BigDecimal) => salary1 + salary2
+        val summedSalaryPerDept = employees.groupBy(_.getDepartment()).mapValues(_.map(_.getYearlyGrossSalary())).mapValues(salaries => new Tuple2(salaries.size, salaries.reduce(salaryAdder)))
+        summedSalaryPerDept.mapValues(salaryCntVsSummedSalary => salaryCntVsSummedSalary._2 / BigDecimal.valueOf(salaryCntVsSummedSalary._1))
+    }
 }
--- CoreScala/simple-scala/src/test/scala/org/gb/sample/scala/collection/PersonOperationsTest.scala
+++ CoreScala/simple-scala/src/test/scala/org/gb/sample/scala/collection/PersonOperationsTest.scala
@@ -10,21 +10,85 @@ import java.math.BigDecimal
 class PersonOperationsTest extends FunSuite {
     val dateFormatter: DateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-    test("people are grouped by mother tongue") {
-        val people = loadPeople()
+    test("people grouped by mother tongue") {
+        val people = loadEmployees()
         val peopleByMotherTongue = PersonOperations.groupByMotherTongue(people)
         val englishPeople = peopleByMotherTongue.get("English");
         val punjabiPeople = peopleByMotherTongue.get("Punjabi");
         val russianPeople = peopleByMotherTongue.get("Russian");
         val konkaniPeople = peopleByMotherTongue.get("Konkani");
-        println(peopleByMotherTongue)
         assertResult(5)(englishPeople.get.size)
         assertResult(2)(punjabiPeople.get.size)
         assertResult(1)(russianPeople.get.size)
         assertResult(1)(konkaniPeople.get.size)
     }
 
-    private def loadPeople(): IndexedSeq[Person] = {
+    test("maximum salary per department") {
+        val employees = loadEmployees()
+        val maxSalaryPerDept = PersonOperations.maxSalaryPerDept(employees)
+        val maxSalaryOfHR = maxSalaryPerDept.get("HR").get
+        val maxSalaryOfDev = maxSalaryPerDept.get("Development").get
+        val maxSalaryOfOps = maxSalaryPerDept.get("Operations").get
+        val maxSalaryOfMktg = maxSalaryPerDept.get("Marketing").get
+        val maxSalaryOfFin = maxSalaryPerDept.get("Finance").get
+        val maxSalaryOfOffMgmt = maxSalaryPerDept.get("Office Management").get
+        val maxSalaryOfIT = maxSalaryPerDept.get("IT").get
+        assertResult(70000)(maxSalaryOfHR.intValue())
+        assertResult(80000)(maxSalaryOfDev.intValue())
+        assertResult(70000)(maxSalaryOfOps.intValue())
+        assertResult(65000)(maxSalaryOfMktg.intValue())
+        assertResult(65000)(maxSalaryOfFin.intValue())
+        assertResult(52000)(maxSalaryOfOffMgmt.intValue())
+        assertResult(46000)(maxSalaryOfIT.intValue())
+    }
+
+    test("employee name with maximum salary per department") {
+        val employees = loadEmployees()
+        val empNameWithMaxSalaryPerDept = PersonOperations.empNameWithMaxSalaryPerDept(employees)
+        assertResult("Kate Avery")(empNameWithMaxSalaryPerDept.get("HR").get);
+        assertResult("Arun Ranganathan")(empNameWithMaxSalaryPerDept.get("Development").get);
+        assertResult("Bonnie MacArthur")(empNameWithMaxSalaryPerDept.get("Operations").get);
+        assertResult("Riina Sulonen")(empNameWithMaxSalaryPerDept.get("Marketing").get);
+        assertResult("Megha Shah")(empNameWithMaxSalaryPerDept.get("Finance").get);
+        assertResult("Kaori Reynolds")(empNameWithMaxSalaryPerDept.get("Office Management").get);
+        assertResult("Faisal Kamal")(empNameWithMaxSalaryPerDept.get("IT").get);
+    }
+
+    test("employee count per sex per department above a base salary") {
+        val employees = loadEmployees()
+        val employeeCountPerSexPerDept = PersonOperations.employeeCountPerSexPerDept(employees, new BigDecimal(30000.00))
+        val empCountPerSexOfHR = employeeCountPerSexPerDept.get("HR").get
+        val empCountPerSexOfDev = employeeCountPerSexPerDept.get("Development").get
+        val empCountPerSexOfOps = employeeCountPerSexPerDept.get("Operations").get
+        val empCountPerSexOfMktg = employeeCountPerSexPerDept.get("Marketing").get
+        val empCountPerSexOfFin = employeeCountPerSexPerDept.get("Finance").get
+        val empCountPerSexOfOffMgmt = employeeCountPerSexPerDept.get("Office Management").get
+        val empCountPerSexOfIT = employeeCountPerSexPerDept.get("IT").get
+        assertResult(1)(empCountPerSexOfHR.get("FEMALE_COUNT").get)
+        assertResult(1)(empCountPerSexOfDev.get("FEMALE_COUNT").get)
+        assertResult(7)(empCountPerSexOfDev.get("MALE_COUNT").get)
+        assertResult(2)(empCountPerSexOfOps.get("FEMALE_COUNT").get)
+        assertResult(2)(empCountPerSexOfOps.get("MALE_COUNT").get)
+        assertResult(2)(empCountPerSexOfMktg.get("FEMALE_COUNT").get)
+        assertResult(1)(empCountPerSexOfMktg.get("MALE_COUNT").get)
+        assertResult(1)(empCountPerSexOfFin.get("FEMALE_COUNT").get)
+        assertResult(1)(empCountPerSexOfOffMgmt.get("FEMALE_COUNT").get)
+        assertResult(2)(empCountPerSexOfIT.get("MALE_COUNT").get)
+    }
+
+    test("average salary per department") {
+        val employees = loadEmployees()
+        val avgSalaryPerDept = PersonOperations.averageSalaryPerDepartment(employees)
+        assertResult(70000)(avgSalaryPerDept.get("HR").get.intValue())
+        assertResult(52750)(avgSalaryPerDept.get("Development").get.intValue())
+        assertResult(54000)(avgSalaryPerDept.get("Operations").get.intValue())
+        assertResult(57000)(avgSalaryPerDept.get("Marketing").get.intValue())
+        assertResult(65000)(avgSalaryPerDept.get("Finance").get.intValue())
+        assertResult(52000)(avgSalaryPerDept.get("Office Management").get.intValue())
+        assertResult(45500)(avgSalaryPerDept.get("IT").get.intValue())
+    }
+
+    private def loadEmployees(): IndexedSeq[Employee] = {
         val emp1 = new Employee(firstName = "Laura", lastName = "Edwards", motherTongue = "English",
             sex = Sex.FEMALE, joiningDate = convertToDate("12/05/2013"), jobTitle = "Business Analyst", department = "Operations",
             yearlyGrossSalary = new BigDecimal(45000.00))
@@ -65,20 +129,20 @@ class PersonOperationsTest extends FunSuite {
             sex = Sex.MALE, joiningDate = convertToDate("08/06/2015"), jobTitle = "Developer", department = "Development",
             yearlyGrossSalary = new BigDecimal(42000.00))
         val emp14 = new Employee(firstName = "Riina", lastName = "Sulonen", motherTongue = "English",
-            sex = Sex.FEMALE, joiningDate = convertToDate("11/05/2015"), jobTitle = "Manager", department = "Finance",
+            sex = Sex.FEMALE, joiningDate = convertToDate("18/03/2013"), jobTitle = "Team Lead", department = "Marketing",
             yearlyGrossSalary = new BigDecimal(65000.00))
         val emp15 = new Employee(firstName = "Megha", lastName = "Shah", motherTongue = "Gujrati",
-            sex = Sex.FEMALE, joiningDate = convertToDate("18/03/2013"), jobTitle = "Marketing Specialist", department = "Marketing",
-            yearlyGrossSalary = new BigDecimal(48000.00))
+            sex = Sex.FEMALE, joiningDate = convertToDate("11/05/2015"), jobTitle = "Manager", department = "Finance",
+            yearlyGrossSalary = new BigDecimal(65000.00))
         val emp16 = new Employee(firstName = "Joanna", lastName = "Ashton", motherTongue = "English",
             sex = Sex.FEMALE, joiningDate = convertToDate("20/01/2014"), jobTitle = "Team Lead", department = "Marketing",
-            yearlyGrossSalary = new BigDecimal(65000.00))
+            yearlyGrossSalary = new BigDecimal(48000.00))
         val emp17 = new Employee(firstName = "Kaori", lastName = "Reynolds", motherTongue = "Japanese",
             sex = Sex.FEMALE, joiningDate = convertToDate("10/02/2012"), jobTitle = "Administration Assistant", department = "Office Management",
             yearlyGrossSalary = new BigDecimal(52000.00))
         val emp18 = new Employee(firstName = "Bonnie", lastName = "MacArthur", motherTongue = "English",
             sex = Sex.FEMALE, joiningDate = convertToDate("08/10/2014"), jobTitle = "Mgr, Customer Service", department = "Operations",
-            yearlyGrossSalary = new BigDecimal(52000.00))
+            yearlyGrossSalary = new BigDecimal(70000.00))
         val emp19 = new Employee(firstName = "Chaminda", lastName = "Gunarathne", motherTongue = "Sinhalese",
             sex = Sex.MALE, joiningDate = convertToDate("23/06/2014"), jobTitle = "Information Systems Engineer", department = "IT",
             yearlyGrossSalary = new BigDecimal(45000.00))
